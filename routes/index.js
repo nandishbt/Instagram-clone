85b6590a4f6d31d5fa18e5f99e5bb2a5485f0a45
nandishbt <nandishbt2001@gmail.com>
2024-01-15 16:02:17 +0530

    authentication added

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,76 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+var userModel = require('./users');
+
+const localStrategy = require('passport-local');
+const passport = require('passport');
+passport.use(new localStrategy(userModel.authenticate()));
+
+
+
+
+router.get('/', function(req, res) {
+  res.render('index', {footer: false});
+});
+
+router.get('/login', function(req, res) {
+  res.render('login', {footer: false});
+});
+
+router.get('/feed', isLoggedIn, function(req, res) {
+  res.render('feed', {footer: true});
+});
+
+router.get('/profile',isLoggedIn, function(req, res) {
+  res.render('profile', {footer: true});
+});
+
+router.post('/register',function(req,res){
+  var user = new userModel({
+    username : req.body.username,
+    name : req.body.name,
+    email:req.body.email
+  })
+  userModel.register(user,req.body.password)
+  .then(function(){
+    passport.authenticate('local')(req,res,()=>{
+      res.redirect('/profile')
+    })
+  })
+
+})
+
+router.post('/login' , passport.authenticate('local',{
+                successRedirect:'/profile',
+                failureRedirect:'/login'
+}),(req,res)=>{})
+
+router.get('/logout',isLoggedIn,(req,res,next)=>{
+    req.logOut(function(err){
+      if(err) {return next(err)}
+
+      res.redirect('/login')
+    })
+})
+
+function isLoggedIn(req,res,next){
+  if(req.isAuthenticated()){
+    return next();
+  }
+  res.redirect('login')
+}
+
+router.get('/search', isLoggedIn,function(req, res) {
+  res.render('search', {footer: true});
+});
+
+router.get('/edit', isLoggedIn,function(req, res) {
+  res.render('edit', {footer: true});
+});
+
+router.get('/upload',isLoggedIn, function(req, res) {
+  res.render('upload', {footer: true});
 });
 
 module.exports = router;
--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,16 @@
-var express = require('express');
-var router = express.Router();
+var mongoose = require('mongoose');
+const plm = require('passport-local-mongoose');
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
-});
+mongoose.connect('mongodb://127.0.0.1:27017/insta-clone');
 
-module.exports = router;
+const userSchema = mongoose.Schema({
+  username : String,
+  name : String,
+  email:String,
+  profilephoto : String,
+  post : [{type:mongoose.Schema.Types.ObjectId , ref :'Post'}]
+})
+
+userSchema.plugin(plm);
+
+module.exports = mongoose.model('User' , userSchema);
